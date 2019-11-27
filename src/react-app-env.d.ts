963ba63495b3db69e55bd0965fc79daf7909ea59
startsUp <startsUp@users.noreply.github.com>
2019-11-27 05:21:54 +0000

    Automated deployment: Wed Nov 27 05:21:54 UTC 2019 2cdcd0f67a4273da1bd2410af74ce249b191c2b1

--- src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "*.glb" {
-    const value: any;
-    export = value;
- }
--- src/threeJS/SceneSubject.ts
+++
@@ -1,4 +0,0 @@
-export default interface SceneSubject {
-    update: Function
-    getSubject(): THREE.Object3D
-}

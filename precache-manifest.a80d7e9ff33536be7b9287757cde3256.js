b7aa4706ae4690b09d6801d47e3f02321cf7563d
startsUp <startsUp@users.noreply.github.com>
2019-11-27 06:55:29 +0000

    Automated deployment: Wed Nov 27 06:55:29 UTC 2019 23b9748b6f2bd4334c0138bfaf752662531439e8

---
+++ precache-manifest.a80d7e9ff33536be7b9287757cde3256.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f718cea04cabee746642166bcecb80f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3948453ba7d9ce59f58c",
+    "url": "/static/css/main.c6dfa776.chunk.css"
+  },
+  {
+    "revision": "6106089b443b9da45160",
+    "url": "/static/js/2.19aa1da8.chunk.js"
+  },
+  {
+    "revision": "3948453ba7d9ce59f58c",
+    "url": "/static/js/main.bdd3fe7f.chunk.js"
+  },
+  {
+    "revision": "8ad1cfe2009bbefe7b78",
+    "url": "/static/js/runtime-main.21d75647.js"
+  },
+  {
+    "revision": "033af3ab9116461e86ab039ec3f464c5",
+    "url": "/static/media/cube.033af3ab.glb"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9cb987a77b048cf059cde43cfe222a9",
+    "url": "/static/media/pic.f9cb987a.png"
+  },
+  {
+    "revision": "37422d28ac5c658ca41ce3e056fae145",
+    "url": "/static/media/pot-stand.37422d28.glb"
+  },
+  {
+    "revision": "c2efe5dca6c8ce58f8747471de8f6166",
+    "url": "/static/media/pot.c2efe5dc.glb"
+  }
+]);
